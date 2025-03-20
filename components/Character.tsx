26f34007cec266ef4519cc359b76a997028707be
Lakori4 <santivalde04@gmail.com>
2025-03-20 23:06:52 +0100

    video #7

--- components/Character.tsx
+++ components/Character.tsx
@@ -1,19 +1,21 @@
 import { FunctionalComponent } from "preact/src/index.d.ts";
 
 type Props = {
-    image: string;
     name: string,
+    image: string;
     status: string,
 }
 
 const Character: FunctionalComponent<Props> = (props) => {
-    return(
-        <div>
-            <h2>{props.name}</h2>
-            <img src={props.image} alt={props.name} />
-            <p><strong>Status:</strong>{props.status}</p>
-        </div>
-    )
+
+  console.log(props.name);
+  return(
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.image} alt={props.name} />
+      <p><strong>Status:</strong>{props.status}</p>
+    </div>
+  )
 }
 
-export default Character
+export default Character;
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,32 +1,26 @@
 import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
 import Axios from "axios";
-import CharacterContainer from "../components/CharactersContainer.tsx";
+import Character from "../components/Character.tsx";
+
 
 type Character = {
   id: string;
   name: string;
   image: string;
-};
-
-type CharacterAPI = {
-  results: Array<{
-    id: string;
-    name: string;
-    image: string;
-  }>;
+  status: string,
 };
 
 type Data = {
-  characters: Character[];
+  results: Character[];
 };
 
 export const handler: Handlers = {
   GET: async (_req: Request, ctx: FreshContext<unknown, Data>) => {
     const url = "https://rickandmortyapi.com/api/character/";
     try {
-      const response = await Axios.get<CharacterAPI>(url);
-      //console.log(response.data.results.map(ch => ch.image))
-      return (ctx.render({ characters: response.data.results }));
+      const response = await Axios.get<Data>(url);
+      //console.log(response.data.results)
+      return (ctx.render({ results: response.data.results }));
     } catch (e) {
       return new Response("Error de API: " + e);
     }
@@ -34,10 +28,17 @@ export const handler: Handlers = {
 };
 
 const Page = (props: PageProps<Data>) => {
-  const characters = props.data.characters
-
+  const characters = props.data.results
+  console.log(characters.map(e => e.status))
   return (
-  <CharacterContainer characters={characters}/>
+    <div>
+      <h1>Rick and Morty Characters</h1>
+      
+      {characters.map(e => (
+       <Character name={e.name} image={e.image} status={e.status} />
+        
+      ))}
+    </div>
   )
 }
 
