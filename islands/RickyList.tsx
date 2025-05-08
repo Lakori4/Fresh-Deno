df12193eba72aade649bf07290fa0f93ee812982
Lakori4 <santivalde04@gmail.com>
2025-05-08 18:14:09 +0200

    Added dynamic list after search is inputed

--- islands/RickyList.tsx
+++ islands/RickyList.tsx
@@ -4,21 +4,22 @@ import { FunctionalComponent } from "preact/src/index.d.ts";
 const RickyList: FunctionalComponent = () => {
 
   const [characters, setCharacters] = useState<string[]>()
+  const [search, setSearch] = useState<string>()
 
   useEffect(() => {
     getCharacter();
-  })
+  }, [search])
 
   const getCharacter = async () => {
-    const json = await fetch ("https://rickandmortyapi.com/api/character/");
+    const json = await fetch (`https://rickandmortyapi.com/api/character?name=${search}`);
     const data = await json.json()
-    setCharacters(data.results.map(e => e.name))
+    setCharacters(data.results.map((e) => e.name))
     console.log(characters);
   }
 
   return (
     <div>
-      
+      <input type="text" name="nombre" placeholder="Nombre" value={search} onInput={(e) => setSearch(e.currentTarget.value)}/>
       <ul>
         {characters?.map(e => <li>{e}</li>)}  
       </ul>
