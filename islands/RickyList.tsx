a88dafd74e54bf02e23a7606d470b0ecc5875d20
Lakori4 <santivalde04@gmail.com>
2025-05-08 23:35:06 +0200

    Added buttons and set initial values for all states variables

--- islands/RickyList.tsx
+++ islands/RickyList.tsx
@@ -1,25 +1,53 @@
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 import { FunctionalComponent } from "preact/src/index.d.ts";
 
 const RickyList: FunctionalComponent = () => {
 
-  const [characters, setCharacters] = useState<string[]>()
-  const [search, setSearch] = useState<string>()
+  const [characters, setCharacters] = useState<string[]>([("")])
+  const [search, setSearch] = useState<string>("")
+  const [page, setPage] = useState<number>(1)
+  const timeout = useRef<any> (undefined)
+
+  const getCharacter = async () => {
+    const json = await fetch (`https://rickandmortyapi.com/api/character?name=${search}&page=${page}`);
+    const data = await json.json()
+    console.log("characters");
+    setCharacters(data.results.map((e) => e.name))
+    
+  }
+
+
+  const pgUp = () => {
+    if (page && page < 42) {
+      setPage(page + 1)
+    }
+  }
+
+    const pgDn = () => {
+    if (page && page > 0) {
+      setPage(page - 1)
+    }
+  }
 
   useEffect(() => {
     getCharacter();
-  }, [search])
+  }, [search, page])  
 
-  const getCharacter = async () => {
-    const json = await fetch (`https://rickandmortyapi.com/api/character?name=${search}`);
-    const data = await json.json()
-    setCharacters(data.results.map((e) => e.name))
-    console.log(characters);
-  }
+
+  useEffect(() => {
+    if (timeout) clearTimeout(timeout.current)
+    timeout.current= setTimeout(getCharacter, 1000)
+  }, [search]);
 
   return (
     <div>
+
+
+      
       <input type="text" name="nombre" placeholder="Nombre" value={search} onInput={(e) => setSearch(e.currentTarget.value)}/>
+      <br />
+      <button type="button" onClick={pgDn}>Down page</button>
+      <button type="button" onClick={pgUp}>Next page</button>
       <ul>
         {characters?.map(e => <li>{e}</li>)}  
       </ul>
